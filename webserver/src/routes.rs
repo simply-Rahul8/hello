584a752b8d7f20e516b4c3318267912fe61be551
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-08-21 18:04:27 +0200

    feat: add user authentication

--- webserver/src/routes.rs
+++ webserver/src/routes.rs
@@ -2,15 +2,27 @@ use actix_web::web;
 
 use crate::{
     chat::{chat_route, get_count},
-    handlers::{auth_handler, task_handler},
+    handlers::{task_handler, user_handler},
 };
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v1")
-            .service(task_handler::create_task)
-            .service(auth_handler::register),
-    )
-    .route("/count", web::get().to(get_count))
-    .route("/ws", web::get().to(chat_route));
+            .configure(task_routes)
+            .configure(auth_routes)
+            .route("/count", web::get().to(get_count))
+            .route("/ws", web::get().to(chat_route)),
+    );
+}
+
+fn task_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/tasks").service(task_handler::create_task));
+}
+
+fn auth_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/users")
+            .service(user_handler::register)
+            .service(user_handler::login), // Add more auth-related routes here
+    );
 }
