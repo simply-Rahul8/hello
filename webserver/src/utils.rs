8a3c43350722ecaf787d288e4eafea28ded341d3
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-08-21 18:04:49 +0200

    chore: remove unused files

--- webserver/src/utils.rs
+++
@@ -1,9 +0,0 @@
-pub fn hash_password(password: &str) -> String {
-    // Implement password hashing
-    password.to_string()
-}
-
-pub fn verify_password(hash: &str, password: &str) -> bool {
-    // Implement password verification
-    hash == password
-}
