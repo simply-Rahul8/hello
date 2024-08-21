cb1b1778e89de6f693638db573788884ebe4b960
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-08-21 18:09:59 +0200

    refactor: add debug

--- webserver/src/models/user.rs
+++ webserver/src/models/user.rs
@@ -2,7 +2,7 @@ use crate::schema::users;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable, Serialize, Deserialize)]
+#[derive(Queryable, Selectable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
@@ -11,7 +11,7 @@ pub struct User {
     pub password_hash: String,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Debug)]
 #[diesel(table_name = users)]
 pub struct NewUser<'a> {
     pub username: &'a str,
