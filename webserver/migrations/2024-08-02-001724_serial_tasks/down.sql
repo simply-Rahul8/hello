a623b60bacecf349e80d0434202558a2b5c301cc
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-08-21 18:06:01 +0200

    feat: add user id sequence

--- webserver/migrations/2024-08-02-001724_serial_tasks/down.sql
+++ webserver/migrations/2024-08-02-001724_serial_tasks/down.sql
@@ -1,5 +1,7 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE "tasks" ALTER COLUMN "id" DROP DEFAULT;
+ALTER TABLE "users" ALTER COLUMN "id" DROP DEFAULT;
 
 -- Drop the sequence
 DROP SEQUENCE tasks_id_seq;
+DROP SEQUENCE users_id_seq;
--- webserver/migrations/2024-08-02-001724_serial_tasks/up.sql
+++ webserver/migrations/2024-08-02-001724_serial_tasks/up.sql
@@ -2,3 +2,7 @@
 CREATE SEQUENCE tasks_id_seq;
 ALTER TABLE "tasks" ALTER COLUMN "id" SET DEFAULT nextval('tasks_id_seq');
 SELECT setval('tasks_id_seq', (SELECT MAX(id) FROM tasks));
+
+CREATE SEQUENCE users_id_seq;
+ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT nextval('users_id_seq');
+SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
