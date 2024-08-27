ef773506b35559d92b030d235627c021c66297d9
Ibrahim <Ibrahim@ebs.net.pk>
2024-08-27 12:46:42 +0200

    feat: Update task_handler.rs to include GET endpoint for retrieving tasks

--- webserver/src/handlers/task_handler.rs
+++ webserver/src/handlers/task_handler.rs
@@ -1,5 +1,5 @@
 use crate::db::DbPool;
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use serde::Deserialize;
 
 use crate::services::task_service;
@@ -23,5 +23,14 @@ pub async fn create_task(
     match task_service::create_task(&mut conn, &task.description, &task.reward).await {
         Ok(task) => HttpResponse::Ok().json(task),
         Err(_) => HttpResponse::InternalServerError().json("Error creating new task"),
+    }
+}
+
+#[get("")]
+pub async fn get_tasks(pool: web::Data<DbPool>) -> impl Responder {
+    let mut conn = pool.get().expect("Failed to get DB connection.");
+    match task_service::get_tasks(&mut conn).await {
+        Ok(tasks) => HttpResponse::Ok().json(tasks),
+        Err(_) => HttpResponse::InternalServerError().json("Error getting tasks"),
     }
 }
