dd8d9f382424150fc80ed8d1fba3723e91388ab4
Ibrahim <Ibrahim@ebs.net.pk>
2024-08-27 12:13:44 +0200

    refactor: Update task_handler.rs to conform to RESTful standard

--- webserver/src/handlers/task_handler.rs
+++ webserver/src/handlers/task_handler.rs
@@ -10,7 +10,7 @@ pub struct CreateTaskRequest {
     reward: i64,
 }
 
-#[post("/tasks")]
+#[post("")]
 pub async fn create_task(
     pool: web::Data<DbPool>,
     task: web::Json<CreateTaskRequest>,
