8ffbaffad50b9a07e2198e9b513f930cdcb2a645
Kareem <kareemhassan95@hotmail.com>
2025-01-31 10:05:41 +0100

    feature/terms-and-conditions and privacy-policy pages (#102)
    Added 'terms and conditions' and 'privacy-policy' pages

--- frontend/app/components/layout/InfoPageHeader.tsx
+++ frontend/app/components/layout/InfoPageHeader.tsx
@@ -15,13 +15,13 @@ const InfoPageHeader: React.FC<InfoPageHeaderProps> = ({ title }) => {
     
     return (
         <div className="flex flex-1 flex-col p-6 bg-gradient-to-r from-[#1C1819] to-[#BD71D4] rounded-lg
-            max-w-full h-[350px] md:h-[459px] px-6 md:px-[80px] pt-[50px] md:pt-[80px] pb-[12px] md:pb-[24px] space-y-12 lg:space-y-28">
+            max-w-full h-[240px] md:h-[459px] px-6 md:px-[80px] pt-6 md:pt-[80px] pb-[24px] space-y-14 lg:space-y-28">
             {/* Page Title */}
-            <h1 className="font-montserrat text-xl md:text-3xl lg:text-6xl font-bold text-white text-center">{title}</h1>
+            <h1 className="font-montserrat text-2xl md:text-4xl lg:text-6xl font-bold text-white text-center">{title}</h1>
         
             <div className="flex flex-col items-center h-full justify-between">
                 {/* Navigation Links */}
-                <div className="flex md:flex-row flex-col sm:text-sm md:text-lg lg:text-2xl justify-center gap-5 md:gap-[160px]">
+                <div className="flex flex-row text-sm md:text-lg lg:text-2xl justify-center gap-4 md:gap-[160px]">
                     <a href="/privacy-policy"
                         className={`block font-montserrat text-white ${isActive('/privacy-policy') ? 'font-bold border-b-2 md:border-b-3 pb-3' : ''} hover:underline`}
                     >
@@ -33,15 +33,16 @@ const InfoPageHeader: React.FC<InfoPageHeaderProps> = ({ title }) => {
                     Terms & Conditions
                     </a>
                     <a href="/contact-us"
-                        className={`block font-montserrat text-white ${isActive('/contact') ? 'font-bold border-b-2 md:border-b-3 pb-3' : ''} hover:underline`}
+                        className={`block font-montserrat text-white ${isActive('/contact-us') ? 'font-bold border-b-2 md:border-b-3 pb-3' : ''} hover:underline`}
                     >
                     Contact Us
                     </a>
 
                 </div>
-                {/* Today's Date */}
-                <div className="text-white text-sm md:text-base font-open-sans self-start">Effective date: {today}</div>
             </div>
+
+            {/* Today's Date */}
+            <div className="text-white text-xs md:text-base font-open-sans self-start">Effective date: {today}</div>
         </div>
     );
 };
